6c75f78dd74c572d93f5b37022bde0db0966db9a
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-09-04 15:56:47 +0300

    zadacha4

---
+++ cp4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class cp4 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int Pr = random.nextInt(10000);
+
+        Map<Integer, Integer> votes = new HashMap<>();
+
+        for (int i = 0; i < Pr; i++) {
+            int pairNumber = random.nextInt(16) + 1;
+            votes.put(pairNumber, votes.getOrDefault(pairNumber, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> sortedVotes = new ArrayList<>(votes.entrySet());
+        sortedVotes.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : sortedVotes) {
+            if (count >= 3) {
+                break;
+            }
+            int teamNumber = entry.getKey();
+            int voteCount = entry.getValue();
+            System.out.println("Пара под номером " + teamNumber + " набрала " + voteCount + " голосов!");
+            count++;
+        }
+    }
+}

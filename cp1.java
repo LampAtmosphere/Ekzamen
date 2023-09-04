a45e73fa02cfb29fcbf2f72886ae96efcc9e8d8a
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-09-04 14:06:34 +0300

    Cp1

--- cp1.java
+++ cp1.java
@@ -1,26 +1,38 @@
 import java.util.Random;
-
 public class cp1 {
     public static void main(String[] args) {
         Random random = new Random();
-        int size = 1000;
-        int[] mac1 = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            mac1[i] = random.nextInt(10001);
-        }
-
-        int max_R = -1;
-
-        for (int i = 0; i < mac1.length; i++) {
-            for (int j = i + 1; j < mac1.length; j++) {
-                int product = mac1[i] * mac1[j];
-                if (product % 14 == 0 && product > max_R && product <= 10000) {
-                    max_R = product;
-                }
+        int[] num = new int[1000];
+        int max = 0;
+        int max2 = 0;
+        int max7 = 0;
+        int max14 = 0;
+        int otv1;
+        int otv2;
+        for(int i = 0;i < num.length;i++){
+            num[i] = random.nextInt(10000);// массив рандомных знач.(предел 10000)
+            if (num[i] > max) {
+                max = num[i];
+            }
+            if ((num[i] % 2 == 0) && (num[i] > max2)) {
+                max2 = num[i];
+            }
+            if ((num[i] % 7 == 0) && (num[i] > max7)) {//ищем кратные 2 ; 7 ; 14
+                max7 = num[i];
+            }
+            if ((num[i] % 14 == 0) && (num[i] > max14)) {
+                max14 = num[i];
             }
         }
 
-        System.out.println("Максимальное число R: " + max_R);
+        otv1 = max * max14;
+        otv2 = max2 * max7;
+
+        if (otv1 == 0 && otv2 == 0){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(Math.max(otv1, otv2));//ищем макс число
+        }
     }
 }

71758f715dce5438f9482cc320b5c3a3d1dfca1d
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-11-02 13:08:08 +0300

    Konec

--- CP/System_mus/System_musik.java
+++ CP/System_mus/System_musik.java
@@ -6,6 +6,8 @@ import org.jsoup.select.Elements;
 
 public class System_musik {
 
+    public static String Url;
+
     private static final String IN_FILE_TXT = "C:\\Users\\datch\\OneDrive\\Документы\\GitHub\\Ekzamen\\CP\\System_mus\\inFile.txt";
     private static final String OUT_FILE_TXT = "C:\\Users\\datch\\OneDrive\\Документы\\GitHub\\Ekzamen\\CP\\System_mus\\outFile.txt";
     private static final String PATH_TO_MUSIC = "C:\\Users\\datch\\OneDrive\\Документы\\GitHub\\Ekzamen\\CP\\System_mus\\music\\";
@@ -13,7 +15,37 @@ public class System_musik {
     private static final String FOR_IMG_FILE = "C:\\Users\\datch\\OneDrive\\Документы\\GitHub\\Ekzamen\\CP\\System_mus\\ForImgFile.txt";
     private static final String FOR_OUT_IMG = "C:\\Users\\datch\\OneDrive\\Документы\\GitHub\\Ekzamen\\CP\\System_mus\\ForOutImg.txt";
     public static void main(String[] args) {
-        String Url;
+        new Thread(() -> {
+            try (BufferedReader inFile = new BufferedReader(new FileReader(FOR_IMG_FILE));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(FOR_OUT_IMG))) {
+                while ((Url = inFile.readLine()) != null) {
+                Document doc = Jsoup.connect(Url).get();
+                Elements imgEl = doc.select("a[href$=.jpg]");
+                for (Element ImgElem : imgEl) {
+                    String imgUrl = ImgElem.attr("href");
+                    outFile.write(imgUrl + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            try (BufferedReader imgFile = new BufferedReader(new FileReader(FOR_OUT_IMG))) {
+            String img;
+            int count = 0;
+            try {
+                while ((img = imgFile.readLine()) != null) {
+                    String imgfilepath = PATH_TO_IMG + String.valueOf(count) + ".jpg";
+                    MusicandImgDownloader.downloadImage(img, imgfilepath);
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }).start();
+        
         try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
              BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
             while ((Url = inFile.readLine()) != null) {
@@ -28,19 +60,7 @@ public class System_musik {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (BufferedReader inFile = new BufferedReader(new FileReader(FOR_IMG_FILE));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(FOR_OUT_IMG))) {
-                while ((Url = inFile.readLine()) != null) {
-                Document doc = Jsoup.connect(Url).get();
-                Elements imgEl = doc.select("a[href$=.jpg]");
-                for (Element ImgElem : imgEl) {
-                    String imgUrl = ImgElem.attr("href");
-                    outFile.write(imgUrl + "\n");
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+         
         try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
             String music;
             int count = 0;
@@ -57,22 +77,5 @@ public class System_musik {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    
-
-        try (BufferedReader imgFile = new BufferedReader(new FileReader(FOR_OUT_IMG))) {
-                    String img;
-                    int count = 0;
-                    try {
-                        while ((img = imgFile.readLine()) != null) {
-                            String imgfilepath = PATH_TO_IMG + String.valueOf(count) + ".jpg";
-                            MusicandImgDownloader.downloadImage(img, imgfilepath);
-                            count++;
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
     }
 }
